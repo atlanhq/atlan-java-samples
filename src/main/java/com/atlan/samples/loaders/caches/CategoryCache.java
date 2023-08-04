2ef11d0fa7be477dc717405e2cd35076558b6589
Christopher Grote <cmgrote@users.noreply.github.com>
2023-08-04 15:39:51 +0100

    Fixes issues with category-handling
    Signed-off-by: Christopher Grote <cmgrote@users.noreply.github.com>

--- src/main/java/com/atlan/samples/loaders/caches/CategoryCache.java
+++ src/main/java/com/atlan/samples/loaders/caches/CategoryCache.java
@@ -3,9 +3,13 @@
 package com.atlan.samples.loaders.caches;
 
 import com.atlan.exception.AtlanException;
+import com.atlan.exception.NotFoundException;
 import com.atlan.model.assets.Asset;
+import com.atlan.model.assets.Glossary;
 import com.atlan.model.assets.GlossaryCategory;
+import com.atlan.model.assets.IGlossaryCategory;
 import com.atlan.samples.loaders.models.CategoryEnrichmentDetails;
+import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -13,16 +17,38 @@ public class CategoryCache extends AssetCache {
 
     /** {@inheritDoc} */
     protected Asset lookupAsset(String identity) {
-        String categoryPath = CategoryEnrichmentDetails.getPathFromIdentity(identity);
-        String categoryName = CategoryEnrichmentDetails.getNameFromPath(categoryPath);
         String glossaryName = CategoryEnrichmentDetails.getGlossaryNameFromIdentity(identity);
         try {
-            // TODO: technically there can be multiple categories with the same name in a
-            //  given glossary, so we really need a findByPath() method instead
-            return GlossaryCategory.findByName(categoryName, glossaryName, null);
+            // Since there can be multiple categories with the same name, and building up an
+            // entire category path is a recursive operation, it'll likely be best if we just
+            // bulk-load the entire hierarchy from a glossary for caching purposes
+            Asset foundCategory = null;
+            Glossary glossary = Glossary.findByName(glossaryName, List.of("name"));
+            Glossary.CategoryHierarchy hierarchy = glossary.getHierarchy(List.of("anchor"));
+            for (IGlossaryCategory category : hierarchy.breadthFirst()) {
+                String categoryPath = getCategoryPath(hierarchy, category);
+                String categoryId = CategoryEnrichmentDetails.getIdentity(categoryPath, glossary);
+                put(categoryId, (Asset) category);
+                if (categoryId.equals(identity)) {
+                    foundCategory = (Asset) category;
+                }
+            }
+            return foundCategory;
+        } catch (NotFoundException e) {
+            log.info("No existing categories found in glossary: {}", glossaryName);
         } catch (AtlanException e) {
             log.error("Unable to lookup or find category: {}", identity, e);
         }
         return null;
+    }
+
+    private String getCategoryPath(Glossary.CategoryHierarchy hierarchy, IGlossaryCategory category) {
+        String parentPath = null;
+        if (category.getParentCategory() != null) {
+            GlossaryCategory parent =
+                    hierarchy.getCategory(category.getParentCategory().getGuid());
+            parentPath = getCategoryPath(hierarchy, parent);
+        }
+        return parentPath == null ? category.getName() : parentPath + "@" + category.getName();
     }
 }
